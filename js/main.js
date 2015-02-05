b233e709fc532eb9e9a0de404aafc2310849494e
Meatloose <poiuy_1100@163.com>
2015-02-05 15:52:49 +0800

    First commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+require.config({
+  baseUrl: 'js',
+  paths: {
+    jquery: 'jquery.min',
+    sha1: 'sha1',
+    weixin: 'jweixin-1.0.0',
+    wechat: 'wechat.min'
+  },
+  shim: {
+    wechat: {
+      deps: ['jquery', 'sha1', 'weixin']
+    }
+  }
+});
+require(['wechat', 'weixin'], function (wechat, wx) {
+  var link = '../wechat/test',//获取jsapi_ticket接口
+    a = 'wxf8b4f85f3a794e77';//appId
+  $.when(wechat.getTicket(link)).done(function (d) {
+    var j = d.ticket,
+      n = wechat.randomWord(),//nonceStr
+      t = wechat.timeStamp(),//timestamp
+      u = window.location.href,//url
+      s = wechat.string1(j, n, t, u);//signature
+    console.log(j, n, t, u, s);
+    wechat.config(a, t, n, s);
+    wx.ready(function () {
+      wechat.shareQQ();
+      wechat.shareTimeline();
+      wechat.shareAppMessage();
+      wechat.shareWeibo();
+    });
+  });
+})
